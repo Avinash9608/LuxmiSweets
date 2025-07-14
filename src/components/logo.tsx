90663bceab9ccb139d1b8bd69b746f7a738d4fc4
Manorama <m3361555@gmail.com>
2025-07-14 06:38:06 +0000

    cake type logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -10,18 +10,23 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <path
-        d="M12 2L5.87868 8.12132C4.70711 9.29289 4.70711 11.2321 5.87868 12.4037L12 18.525L18.1213 12.4037C19.2929 11.2321 19.2929 9.29289 18.1213 8.12132L12 2Z"
-        className="text-primary"
-        fill="currentColor"
-      />
-      <path
-        d="M12 2L5.87868 8.12132C4.70711 9.29289 4.70711 11.2321 5.87868 12.4037L12 18.525"
-        className="text-accent"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeLinejoin="round"
-      />
+        <path
+            d="M20 21v-8a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v8"
+            className="text-primary/70"
+            fill="currentColor"
+            stroke="currentColor"
+            strokeWidth="1"
+        />
+        <path
+            d="M4 16c-1.1 0-2 .9-2 2v3h20v-3c0-1.1-.9-2-2-2H4Z"
+            className="text-primary"
+            fill="currentColor"
+        />
+        <path
+            d="M12 4c-1.1 0-2 .9-2 2v2a2 2 0 1 0 4 0V6c0-1.1-.9-2-2-2Z"
+            className="text-accent"
+            fill="currentColor"
+        />
     </svg>
   );
 }
