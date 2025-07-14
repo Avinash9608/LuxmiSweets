501e439646a07dc831f12265146e33d6baf5b08b
Manorama <m3361555@gmail.com>
2025-07-14 11:00:23 +0000

    change the footer section color

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -36,7 +36,7 @@ export function Footer() {
 
   return (
     <>
-      <footer className="bg-secondary text-secondary-foreground">
+      <footer className="bg-card text-secondary-foreground border-t">
         <div className="container mx-auto py-12 px-4 md:px-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           
           {/* Brand Identity */}
